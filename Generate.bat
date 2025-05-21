c0e5e94ea34d5a34c822f7a4e721af08fe08e9e7
ArnaudDroxler <arnaud.droxler@gmail.com>
2025-05-21 17:45:10 +0200

    Add premake script and create vs solution + Add spdlog & imgui

---
+++ Generate.bat
@@ -0,0 +1,2 @@
+call External\Premake\premake5.exe vs2022
+PAUSE
---
+++ premake5.lua
@@ -0,0 +1,98 @@
+workspace "AuroraRenderer"
+	architecture "x64"
+	startproject "Sandbox"
+    configurations
+	{
+		"Debug",
+		"Release"
+	}
+
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}"
+
+IncludeDir = {}
+IncludeDir["ImGui"] = "AuroraRenderer/external/imgui"
+
+group "Dependencies"
+	include "AuroraRenderer/external/imgui"
+
+group ""
+
+project "AuroraRenderer"
+	location "AuroraRenderer"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++23"
+	staticruntime "on"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-obj/" .. outputdir .. "/%{prj.name}")
+
+	pchheader "aurorapch.h"
+	pchsource "AuroraRenderer/src/aurorapch.cpp"
+
+    files { "%{prj.name}/src/**.h", "%{prj.name}/src/**.cpp" }
+	
+	includedirs
+	{
+		"%{prj.name}/src",
+		"%{prj.name}/external/spdlog/include",
+		"%{IncludeDir.ImGui}",
+	}
+
+		
+	defines
+	{
+		"_CRT_SECURE_NO_WARNINGS"
+	}
+
+	
+	links 
+	{ 
+		"ImGui",
+	}
+
+	filter "configurations:Debug"
+		defines "AURORA_DEBUG"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines "AURORA_RELEASE"
+		runtime "Release"
+		optimize "on"
+
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++23"
+	staticruntime "on"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-obj/" .. outputdir .. "/%{prj.name}")
+
+    files { "%{prj.name}/src/**.h", "%{prj.name}/src/**.cpp" }
+	
+	includedirs
+	{
+		"AuroraRenderer/src",
+		"AuroraRenderer/external/spdlog/include",
+		"AuroraRenderer/external",
+	}
+
+	links 
+	{
+		"AuroraRenderer"
+	}
+		
+	filter "configurations:Debug"
+		defines "AURORA_DEBUG"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines "AURORA_RELEASE"
+		runtime "Release"
+		optimize "on"
